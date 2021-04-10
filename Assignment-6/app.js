e48986d67423eea04c6e0545229c9d05e038cb97
Manni Sharma <mannisharma1226@gmail.com>
2021-04-10 19:05:55 +0530

    Assignment 6 (Blog App)

---
+++ Assignment-6/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const articleRouter = require('./routes/articles');
+const Article = require('./models/article');
+const methodoverride = require('method-override');
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/blog', 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex:true
+    })
+    .then(() => {
+        console.log("Database Connected");
+    })
+    .catch(err => {
+        console.log("DB Not Connected");
+        console.log(err);
+    })
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodoverride('_method'));
+
+app.get('/', async(req, res) => {
+
+    const articles =  await Article.find().sort({ createdAt: 'desc' });  
+
+    res.render('articles/index', {articles : articles});
+})
+
+app.use('/articles', articleRouter);
+
+app.listen(3000, () => {
+    console.log("Server Running at port 3000");
+});
---
+++ Assignment-6/models/article.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        required: true,
+        type: String
+    },
+    imageUrl: {
+        type: String
+    },
+    author: {
+        required: true,
+        type: String
+    },
+    description: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model('Article', articleSchema);
